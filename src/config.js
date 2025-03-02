7608537fa74d4dc92349e82d5637d15f22783c7b
adiynsyah <adiyansyahdwiputra@gmail.com>
2025-03-02 13:50:48 +0700

    feat: adjust apiURL Prod

--- src/config.js
+++ src/config.js
@@ -31,10 +31,7 @@ export const AUTH0_API = {
 };
 
 // API Production and UAT
-// const apiUrl = 'https://prod.radhiyanpetandcare.online';
-
-// API Development
-const apiUrl = 'https://uat.radhiyanpetandcare.online';
+const apiUrl = 'https://prod.radhiyanpetandcare.online';
 
 // const apiUrl = process?.env?.REACT_APP_MODE === 'local' ? 'http://localhost:8000' : 'https://uat.radhiyanpetandcare.online/api';
 // API LOCAL
