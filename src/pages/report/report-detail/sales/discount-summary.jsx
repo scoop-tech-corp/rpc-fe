01f46f956c69ed8ef762d8e09b78bad373c46790
Abdul Rizki <abdulrizki2002@gmail.com>
2025-03-14 09:23:22 +0700

    fix(report): fix discount summary chart style

--- src/pages/report/report-detail/sales/discount-summary.jsx
+++ src/pages/report/report-detail/sales/discount-summary.jsx
@@ -1,6 +1,10 @@
 import { Box, Grid } from '@mui/material';
+import MainCard from 'components/MainCard';
 import AnalyticEcommerce from 'components/dashboard/card';
-import { useEffect, useMemo } from 'react';
+import ApexColumnChart from 'components/dashboard/column';
+import ApexPieChart from 'components/dashboard/pie';
+import { useMemo } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { formatThousandSeparator } from 'utils/func';
 
 export default function SalesDiscountSummary({ data, filter, setFilter }) {
@@ -18,121 +22,10 @@ export default function SalesDiscountSummary({ data, filter, setFilter }) {
     };
   }, [data]);
 
-  useEffect(() => {
-    const options = {
-      series: pieChart.series,
-      chart: {
-        type: 'donut',
-        width: 750,
-        height: 750
-      },
-      labels: pieChart.labels, // Set reasons as labels for better representation,
-      title: {
-        text: 'Discount value by staff (RP)', // Menambahkan judul
-        align: 'left', // Posisi kiri (tetap di atas, perlu CSS untuk pindah ke samping)
-        offsetX: 20,
-        margin: 20,
-        style: {
-          fontSize: '16px',
-          fontWeight: 'bold'
-        }
-      },
-      responsive: [
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200
-            },
-            legend: {
-              position: 'bottom'
-            }
-          }
-        }
-      ]
-    };
-
-    const chart = new ApexCharts(document.querySelector('#pieChart'), options);
-    chart.render();
-
-    // Cleanup function
-    return () => {
-      chart.destroy();
-    };
-  }, [pieChart]);
-
-  useEffect(() => {
-    // Create the chart options using dummyChartData
-    const options = {
-      series: barChart.series,
-      chart: {
-        height: 350,
-        type: 'bar',
-        dropShadow: {
-          enabled: true,
-          color: '#000',
-          top: 18,
-          left: 7,
-          blur: 10,
-          opacity: 0.2
-        },
-        toolbar: {
-          show: false
-        }
-      },
-      colors: ['#77B6EA', '#545454'],
-      dataLabels: {
-        enabled: true
-      },
-      stroke: {
-        curve: 'smooth'
-      },
-      title: {
-        text: '',
-        align: 'left'
-      },
-      grid: {
-        borderColor: '#e7e7e7',
-        row: {
-          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-          opacity: 0.5
-        }
-      },
-      markers: {
-        size: 1
-      },
-      xaxis: {
-        categories: barChart.categories
-      },
-      yaxis: {
-        title: {
-          text: 'Discount Value (Rp)'
-        }
-        // min: 5,
-        // max: 40
-      },
-      legend: {
-        position: 'top',
-        horizontalAlign: 'center'
-        // floating: true,
-        // offsetY: -25,
-        // offsetX: -5
-      }
-    };
-
-    const chart = new ApexCharts(document.querySelector('#barChart'), options);
-    chart.render();
-
-    // Cleanup function
-    return () => {
-      chart.destroy();
-    };
-  }, [barChart]);
-
   return (
     <div>
       <Box sx={{ padding: 2 }}>
-        <Grid container spacing={3} sx={{ marginBottom: 3 }}>
+        <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={4}>
             <AnalyticEcommerce
               title="Total Discount"
@@ -163,8 +56,17 @@ export default function SalesDiscountSummary({ data, filter, setFilter }) {
         </Grid>
       </Box>
 
-      <div id="barChart" style={{ marginBottom: 20 }} />
-      <div id="pieChart" style={{ marginBottom: 20 }} />
+      <Box sx={{ margin: 2 }}>
+        <MainCard title={<FormattedMessage id="discount-value-rp" />}>
+          <ApexColumnChart categoriesProps={barChart.categories} seriesProps={barChart.series} />
+        </MainCard>
+      </Box>
+
+      <Box sx={{ margin: 2 }}>
+        <MainCard title={<FormattedMessage id="discount-value-by-staff-rp" />} content={false}>
+          <ApexPieChart labelsProps={pieChart.labels} seriesProps={pieChart.series} />
+        </MainCard>
+      </Box>
     </div>
   );
 }
